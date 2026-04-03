fdf6d37956e23f86fcf131985b23b7b87f6454a6
Boychev95 <tsvetomir.boychev@gmail.com>
2026-04-03 11:19:12 +0300

    Add Buildings CRUD views

--- PropertyManagementSystem.Core/Entities/Building.cs
+++ PropertyManagementSystem.Core/Entities/Building.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
-
+        public int Floors { get; set; }
         public ICollection<Apartment> Apartments { get; set; } = new List<Apartment>();
     }
 }
