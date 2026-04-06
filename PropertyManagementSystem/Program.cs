185f95ec17a61843603a71a665d6d5ae9966c766
Boychev95 <tsvetomir.boychev@gmail.com>
2026-04-06 20:34:54 +0300

    Add Identity database setup and initial migrations
    - Added ApplicationDbContext for Identity
    - Added initial Identity migrations (AddIdentity)
    - Updated Program.cs to register ApplicationDbContext and Identity services
    - Updated appsettings.json with correct connection string

--- PropertyManagementSystem/Program.cs
+++ PropertyManagementSystem/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PropertyManagementSystem.Core.Entities;
+using PropertyManagementSystem.Data;
 using PropertyManagementSystem.Infrastructure.Data;
 
 namespace PropertyManagementSystem
@@ -11,57 +12,33 @@ namespace PropertyManagementSystem
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-                                   ?? "Server=(localdb)\\mssqllocaldb;Database=PropertyManagementDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+            // Identity DB
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString, b => b.MigrationsAssembly("PropertyManagementSystem.Infrastructure")));
+                options.UseSqlServer(connectionString));
+
+            // Domain DB
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString,
+                    b => b.MigrationsAssembly("PropertyManagementSystem.Infrastructure")));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
 
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
-                options.Password.RequireDigit = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequireNonAlphanumeric = false;
-                options.User.RequireUniqueEmail = true;
-            })
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders();
-
-            builder.Services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = "/Identity/Account/Login";
-                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-                options.ExpireTimeSpan = TimeSpan.FromDays(14);
-                options.SlidingExpiration = true;
-            });
-
-            builder.Services.AddHealthChecks();
-
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
+            using (var scope = app.Services.CreateScope())
             {
-                try
-                {
-                    using var scope = app.Services.CreateScope();
-                    var services = scope.ServiceProvider;
-                    await DbInitializer.InitializeAsync(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = app.Services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
-                }
+                var services = scope.ServiceProvider;
+                await DbInitializer.InitializeAsync(services);
             }
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
+            if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
@@ -74,8 +51,6 @@ namespace PropertyManagementSystem
 
             app.UseAuthentication();
             app.UseAuthorization();
-
-            app.MapHealthChecks("/health");
 
             app.MapControllerRoute(
                 name: "areas",
