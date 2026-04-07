99e74320ce12b56b9b81f3986ec7ee9c3e6c00cb
Boychev95 <tsvetomir.boychev@gmail.com>
2026-04-07 12:48:07 +0300

    Refactor: Configure ApplicationDbContext for Identity and project structure

--- PropertyManagementSystem/Data/ApplicationDbContext.cs
+++ PropertyManagementSystem/Data/ApplicationDbContext.cs
@@ -4,11 +4,39 @@ using PropertyManagementSystem.Core.Entities;
 
 namespace PropertyManagementSystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Apartment> Apartments { get; set; }
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Building>()
+                .HasMany(b => b.Apartments)
+                .WithOne(a => a.Building)
+                .HasForeignKey(a => a.BuildingId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Apartment>()
+                .HasMany(a => a.Tenants)
+                .WithOne(t => t.Apartment)
+                .HasForeignKey(t => t.ApartmentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Tenant>()
+                .HasMany<Payment>()
+                .WithOne(p => p.Tenant)
+                .HasForeignKey(p => p.TenantId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
